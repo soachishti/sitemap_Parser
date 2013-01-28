a44c82685b1c79c2406639d12958f6ca008fefc9
soac <syedoac@gmail.com>
2013-01-28 18:25:22 +0500

    Uploading Files

--- index.php
+++ index.php
@@ -2,9 +2,17 @@
 include("sitemap_parser.php");
 
 set_time_limit (60);
-$url = 'infocorridor.tk';
+$url = 'example.com';
 $limit = 5;
 $depth = 2;
 fetch($url,$depth,$limit);
-echo $dbpath."";
+$database = $dbpath;
+
+$sql = "SELECT * FROM urls";
+$con = sqlite_open($database, 0666, $error);
+$result = sqlite_query($con,$sql);
+while($row = sqlite_fetch_array($query, SQLITE_ASSOC))
+{
+	print_r($row);
+}  
 ?>
